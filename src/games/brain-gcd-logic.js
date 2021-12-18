1d1c47373a18fa2bd891c276fa860f5f31e36fa3
Тимур Хабибов <Tim-him@mail.ru>
2021-12-18 15:46:58 +0500

    fixed this

--- src/games/brain-gcd-logic.js
+++ src/games/brain-gcd-logic.js
@@ -23,22 +23,6 @@ const findgcd = () => {
   const randomValue2 = randomNumber(0, 99);
   const correctAnswer = getCorrectAnswer(randomValue1, randomValue2);
   const question = `${randomValue1} ${randomValue2}`;
-<<<<<<< HEAD
-  if (randomValue1 === randomValue2) {
-    return `${question};${String(randomValue2)}`;
-  } if (randomValue1 > randomValue2) {
-    maxdel = (randomValue1 / 2);
-  } else {
-    maxdel = (randomValue2 / 2);
-  }
-  for (let del = 1; del < maxdel; del += 1) {
-    if (randomValue1 % del === 0 && randomValue2 % del === 0) {
-      correctAnswer = String(del);
-    }
-  }
-  return `${question};${correctAnswer}`;
-=======
   return [question, correctAnswer];
->>>>>>> 58e8aca (fixed)
 };
 export default () => commonLogic('Find the greatest common divisor of given numbers.', findgcd);
